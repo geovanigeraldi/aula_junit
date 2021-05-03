1c018462ef888e7418f4f6662c8af8079dc01b0a
Geovani Geraldi <geovani.geraldi@gmail.com>
2021-05-03 19:52:19 -0300

    exercicio junit

--- src/aula_junit/entities/Financial.java
+++ src/aula_junit/entities/Financial.java
@@ -3,28 +3,30 @@ package aula_junit.entities;
 public class Financial {
 
 	public static double VALUE_ENTRANCE_PERCENT = 0.2;
-	
-	private Double id;
+
+	private Long id;
 	private Double totalAmount;
 	private Double income;
 	private Integer mounths;
+	private Double renda;
 
 	public Financial() {
 	}
 
-	public Financial(Double id, Double totalAmount, Double income, Integer mounths) {
+	public Financial(Long id, Double totalAmount, Double income, Integer mounths, Double renda) {
 		super();
 		this.id = id;
 		this.totalAmount = totalAmount;
 		this.income = income;
 		this.mounths = mounths;
+		this.renda = renda;
 	}
 
-	public Double getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Double id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -32,12 +34,18 @@ public class Financial {
 		return totalAmount;
 	}
 
+	public void setTotalAmount(Double totalAmount) {
+		this.totalAmount = totalAmount;
+		valideQuotaRenda();
+	}
+	
 	public Double getIncome() {
 		return income;
 	}
 
 	public void setIncome(Double income) {
 		this.income = income;
+		valideQuotaRenda();
 	}
 
 	public Integer getMounths() {
@@ -46,6 +54,31 @@ public class Financial {
 
 	public void setMounths(Integer mounths) {
 		this.mounths = mounths;
+		valideQuotaRenda();
+	}	
+
+	public Double getRenda() {
+		return renda;
 	}
-		
+
+	public void setRenda(Double renda) {
+		this.renda = renda;
+	}
+
+	public double entry() {
+		double e = getTotalAmount() * VALUE_ENTRANCE_PERCENT;
+		return e;
+	}
+
+	public Double quota() {
+		double e = getTotalAmount() - entry();
+		return e / getMounths();
+	}
+	
+	public void valideQuotaRenda() {
+		if(quota() > getRenda()/2) {
+			throw new IllegalArgumentException();
+		}
+	}
+
 }
--- src/tests/entities/FinancialTests.java
+++ src/tests/entities/FinancialTests.java
@@ -1,7 +1,49 @@
 package tests.entities;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import aula_junit.entities.Account;
+import aula_junit.entities.Financial;
+import tests.Factory.AccountFactory;
+
 public class FinancialTests {
 
-	
+	@Test
+	public void entranceShouldTwentyPercenteTotalAmount() {
+		double totalAmount = 1000.0;		
+		double entranceValue = totalAmount - (totalAmount*0.2);
+		double expectedValue = 200.00;
+		double renda = 100.0;
+		
+		Financial f = new Financial(1L, totalAmount, entranceValue, null, renda);
+		Assertions.assertEquals(expectedValue, f.entry());
+	}
+
+	@Test
+	public void quotaValueShouldEightyPercenteTotalAmountSplitedPerMouths() {
+		double totalAmount = 1000.0;		
+		double entranceValue = totalAmount - (totalAmount*0.2);
+		Integer mounths = 12;
+		double expectedValue = 66.66666666666667;
+		double renda = 100.0;
+		
+		Financial f = new Financial(1L, totalAmount, entranceValue, mounths, renda);
+		Assertions.assertEquals(expectedValue, f.quota());
+	}
+
+	@Test
+	public void quotaValueShouldLessHalfRenda() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			double totalAmount = 2000.0;		
+			double entranceValue = totalAmount - (totalAmount*0.2);
+			Integer mounths = 12;
+			double expectedValue = 66.66666666666667;
+			double renda = 100.0;
+			
+			Financial f = new Financial(1L, totalAmount, entranceValue, mounths, renda);
+		});
+	}
 	
 }
